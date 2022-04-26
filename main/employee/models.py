ecd031beadd3e8a27a4b964f9c6b5d64f2f2f8e4
Alex Githui <agithui@dayliff.com>
2022-04-26 15:43:24 +0300

    created employee model

--- main/employee/models.py
+++ main/employee/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class Employee(models.Model):
+    emp_name = models.CharField(max_length=200)
+    emp_email = models.EmailField()
+    emp_contact = models.CharField(max_length=200)
+    emp_role = models.CharField(max_length=200)
+    emp_salary = models.IntegerField()
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return self.emp_name
