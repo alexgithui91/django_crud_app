a7e36d0fa7c5dd33cde0dbc2bcc2fc9258e199f9
Alex Githui <agithui@dayliff.com>
2022-04-26 16:18:11 +0300

    added include import

--- main/main/urls.py
+++ main/main/urls.py
@@ -1,7 +1,7 @@
 # ./main/urls.py
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
