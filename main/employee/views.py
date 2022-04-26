e4c963c04916b0bd09444b6f6f4eee90c6b66b3a
Alex Githui <agithui@dayliff.com>
2022-04-26 15:58:33 +0300

    created views

--- main/employee/views.py
+++ main/employee/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def employees_list(request):
+    return render(request, "employee/list.html", context)
+
+
+def create_employee(request):
+    return render(request, "employee/create.html", context)
+
+
+def edit_employee(request):
+    return render(request, "employee/edit.html", context)
+
+
+def delete_employee(request):
+    return render(request, "employee/delete.html", context)
