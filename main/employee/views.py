bf98261017b8509e1ee632d7b3f33fd2ef914f68
alexgithui91 <alexgithui91@gmail.com>
2022-06-03 18:13:40 +0300

    working list, create, edit views

--- main/employee/views.py
+++ main/employee/views.py
@@ -8,6 +8,7 @@ def employees_list(request):
     context = {
         "employees": employees,
     }
+
     return render(request, "employee/list.html", context)
 
 
@@ -18,13 +19,33 @@ def create_employee(request):
         form = EmployeeForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect()
+            return redirect("employees-list")
+
+    context = {
+        "form": form,
+    }
+
     return render(request, "employee/create.html", context)
 
 
 def edit_employee(request, pk):
+    employee = Employee.objects.get(id=pk)
+    form = EmployeeForm(instance=employee)
+
+    if request.method == "POST":
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect("employees-list")
+
+    context = {
+        "employee": employee,
+        "form": form,
+    }
+
     return render(request, "employee/edit.html", context)
 
 
 def delete_employee(request, pk):
+    context = ""
     return render(request, "employee/delete.html", context)
